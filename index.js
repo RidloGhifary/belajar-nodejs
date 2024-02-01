6e8a0ef73733bcbf770696ab0fe894beeab1e60b
ridho <ridloghfry@gmail.com>
2024-02-01 15:14:10 +0700

    async and promise

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const http = require("http");
+const http = require("node:http");
+const { readFile, writeFile } = require("fs").promises;
+
+const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 const hostname = "127.0.0.1";
 const port = 8000;
@@ -22,3 +27,36 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+const getText = async () => {
+  try {
+    const first = await readFile("./content/text.txt", "utf8");
+    const second = await readFile("./content/text2.txt", "utf8");
+    await writeFile(
+      "./content/writeFromIndex.txt",
+      `Hello these text is from index.js :\n1. ${first}\n2. ${second}\n\n`,
+      { flag: "a" }
+    );
+    console.log(first, second);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getText();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, result) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(result);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/text.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
